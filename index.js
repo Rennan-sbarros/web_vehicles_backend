da4da4694234fa9c7ea136c421926306dbd57959
rennan-sbarros <rennan_sbarros@hotmail.com>
2022-09-10 15:55:28 -0300

    feat: criando projeto e configurando express e banco de dados

---
+++ index.js
@@ -0,0 +1,29 @@
+
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+
+app.use(
+    express.urlencoded({
+      extended: true,
+    }),
+  )
+  
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Você acessou a API com sucesso' })
+})
+
+const DB_USER = process.env.DB_USER
+const DB_PASSWORD = encodeURIComponent(process.env.DB_PASSWORD)
+
+mongoose.connect(
+        `mongodb+srv://${DB_USER}:${DB_PASSWORD}@apicluster.y9xd7vv.mongodb.net/bancodaapi?retryWrites=true&w=majority`,
+    )
+    .then(() => {
+        console.log('Conectou ao banco!')
+        app.listen(3000)
+    })
+    .catch((err) => console.log(err))
